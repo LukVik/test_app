4a8a6fe5cde96090d2836e5a6703a62259751444
LukVik <lukaswvik@gmail.com>
2019-08-12 23:00:39 +0200

    edit mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,16 +62,6 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "test_app_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address: 'smtp.gmail.com',
-      port: 587,
-      domain: 'gmail.com', #you can also use google.com
-      authentication: :plain,
-      user_name: 'lukaswvik@gmail.com',
-      password: 'am00reUK22'
-  }
-
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -99,5 +89,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'lukviktest.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'gmail.com', #you can also use google.com
+      authentication: :plain,
+      user_name: 'lukaswvik@gmail.com',
+      password: 'am00reUK22'
+  }
 end
