affb29bca690918ae9c12a2411798465aaaf7bf9
LukVik <lukaswvik@gmail.com>
2019-08-12 22:32:31 +0200

    edit recaptcha keys

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key="6LdDua8UAAAAAIh9C-PqmZbARIaen0K-jVK-cY4c"
-  config.secret_key="6LdDua8UAAAAADkHVsbhezy8LH_U5egGRrajwGav"
+  config.site_key="6LetrLIUAAAAAFRu73uIbQWbBOsd5I6j4RZSsqqu"
+  config.secret_key="6LetrLIUAAAAABBGlqi9cWJocMO1Q_pDlLPtmsZF"
 end
