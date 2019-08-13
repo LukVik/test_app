f204b65561373a08a25380d5bd1c26858f72626c
LukVik <lukaswvik@gmail.com>
2019-08-13 07:29:50 +0200

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
-      port: 587,
+      port: 465,
       domain: 'gmail.com', #you can also use google.com
       authentication: :plain,
       user_name: 'lukaswvik@gmail.com',
